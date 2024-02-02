484b812ea7defded2274c32207c11e3291b5bf9e
Salaj Malik <salajmalik2000@gmail.com>
2024-02-02 10:53:38 +0530

    created user route and controller

--- src/app.js
+++ src/app.js
@@ -13,7 +13,13 @@ app.use(cors({
 app.use(express.json({limit: "16kb"})) //when data is sent through forms
 app.use(express.urlencoded({extended: true, limit: "16kb"}))  //URL data
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
 export { app}
