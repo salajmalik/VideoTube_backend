845906127dd17678e252b5367f3e028ea42880a6
Salaj Malik <salajmalik2000@gmail.com>
2024-02-06 17:22:29 +0530

    Added all the user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser, loginUser, logoutUser, refreshAccessToken} from "../controllers/user.controller.js";
+import {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser,updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -10,7 +10,16 @@ router.route("/register").post(upload.fields([{name: "avatar", maxCount: 1},{nam
 router.route("/login").post(loginUser)
 
 //secured Routes (routes after logging in)
-router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser) //verifyJWT verifies that the user is loggedIn
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)// if you use post instead of patch then all the details will get updated
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile) //this route is named like this specifically because user.params is being used inside the method
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
