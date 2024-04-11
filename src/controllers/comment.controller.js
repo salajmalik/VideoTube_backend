9e72a2ea4f0be93df1c6ed75b981cb35034d2012
Salaj Malik <salajmalik2000@gmail.com>
2024-04-11 17:03:29 +0530

    Pushing the updates in remaining controlers

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,6 +8,83 @@ const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
+    if(!videoId){
+        throw new ApiError(400,"videoId is required!!");
+    }
+    const video = await Video.findById(videoId);
+    if(!video){
+         // If the video doesn't exist, delete all comments associated with the video ID
+         await Comment.deleteMany({ video: videoId });
+         throw new ApiError(400, "There is no such Video. All associated comments have been deleted.");
+    }
+    const commentsAggregate = Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId),
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+            },
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "comment",
+                as: "likes",
+            },
+        },
+        {
+            $addFields: {
+                likesCount: {
+                    $size: "$likes",
+                },
+                owner: {
+                    $first: "$owner",
+                },
+                isLiked: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$likes.likedBy"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            },
+        },
+        {
+            $project: {
+                content: 1,
+                createdAt: 1,
+                likesCount: 1,
+                owner: {
+                    username: 1,
+                    fullName: 1,
+                    "avatar.url": 1,
+                },
+                isLiked: 1
+            },
+        },
+    ]);
+
+    const options = {
+        page: parseInt(page, 10),
+        limit: parseInt(limit, 10),
+    };
+    const comments = await Comment.aggregatePaginate(
+        commentsAggregate,
+        options
+    );
+
+    if(!comments || comments.length === 0){
+        return res.status(200).json(new ApiResponse(200,{},"No commments in this video!!"))
+    }
+
+    return res.status(200).json(new ApiResponse(200,comments,"Comments of the video fetched Successfully"))
 
 })
 
